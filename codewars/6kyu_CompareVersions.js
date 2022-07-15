0df2ae3a6d1b078493ba3e12809cc6e0f28bf156
InGodWeTrustt <fanat2016@mail.ru>
2022-07-15 15:09:31 +0300

    compare versions in codewars

--- codewars/6kyu_CompareVersions.js
+++ codewars/6kyu_CompareVersions.js
@@ -13,11 +13,13 @@ function compareVersions(v1, v2) {
         splitV2 = splitV2.map((elem, i) => i > 0 && elem.length < 2 ? `0${elem}` : elem).join('.')
         return parseFloat(splitV1) >= parseFloat(splitV2)
     }
-    return splitV1[0] === splitV2[0] ? compareVersions(splitV1.slice(1).join('.'), splitV2.slice(1).join('.') ) : splitV1[0] >= splitV2[0]
+    return splitV1[0] === splitV2[0] ? compareVersions(splitV1.slice(1).join('.'), splitV2.slice(1).join('.') ) : +splitV1[0] >= +splitV2[0]
 
 }
 
-console.log(compareVersions('10.9', '10.10')); //false
-console.log(compareVersions("10.4.6", "10.4")); // true
-console.log(compareVersions("10.4.9", "10.4.6")); // true
-console.log(compareVersions("10.4", "11")); // false
+console.log(compareVersions('124.69.43.6.167', '124.69.136.2')) //false
+// console.log(compareVersions('5.144.3.164', '5.144.90.1')) //false
+// console.log(compareVersions('10.9', '10.10')); //false
+// console.log(compareVersions("10.4.6", "10.4")); // true
+// console.log(compareVersions("10.4.9", "10.4.6")); // true
+// console.log(compareVersions("10.4", "11")); // false
