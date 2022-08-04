231d52cf0152554f7c5a88c7080ebd00f59dbcba
InGodWeTrustt <fanat2016@mail.ru>
2022-08-04 13:40:24 +0300

    highest scoring word

---
+++ codewars-Solutions/6 kyu/6 kyu Highest Scoring Word.js
@@ -0,0 +1,19 @@
+/**
+ Учитывая строку слов, вам нужно найти слово с наивысшим баллом.
+Каждая буква слова оценивается в баллах в соответствии с ее положением в алфавите: a = 1, b = 2, c = 3и т. д.
+Вам нужно вернуть слово с наивысшим баллом в виде строки.
+Если два слова оцениваются одинаково, верните слово, которое появляется первым в исходной строке.
+Все буквы будут строчными, и все входные данные будут действительными.
+ */
+
+function high(x){
+    const split = x.split(' ').map( word => {
+        const sumofNum = [...word].map(elem => elem.codePointAt()-96).reduce((prev, cur) => prev + cur)
+        return [word, sumofNum]
+    })
+    return split.sort((a, b) => b[1]-a[1])[0][0]
+}
+
+console.log();
+
+console.log(high('what time are we climbing up the volcano')); // volcano 
