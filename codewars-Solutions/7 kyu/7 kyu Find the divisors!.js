841f0cde8f243670c6f6616961e5ec4d01eb6e0d
InGodWeTrustt <fanat2016@mail.ru>
2022-12-09 13:41:42 +0300

    6 kyu Tribonacci Sequence.js

---
+++ codewars-Solutions/7 kyu/7 kyu Find the divisors!.js
@@ -0,0 +1,14 @@
+/**
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+ */
+
+
+function divisors(integer) {
+  
+};
+
+console.log(divisors(15), [3, 5]);
+console.log(divisors(12), [2, 3, 4, 6]);
+console.log(divisors(13), "13 is prime");
