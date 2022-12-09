10c6ce64e03da5d1a1b696a98ea1f90ed537ac35
InGodWeTrustt <fanat2016@mail.ru>
2022-12-09 22:33:59 +0300

    8 kyu Invert values.js

---
+++ codewars-Solutions/8 kyu/8 kyu Invert values.js
@@ -0,0 +1,6 @@
+function invert(array) {
+    return array.map(i => -i);
+}
+
+console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
