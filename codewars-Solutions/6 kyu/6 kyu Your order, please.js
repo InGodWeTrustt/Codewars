3bab4bbb01a6eaf6a354240dc6485e44e670fbf8
Evgenii <73018116+InGodWeTrustt@users.noreply.github.com>
2022-12-11 15:57:10 +0300

    Update 6 kyu Your order, please.js

--- codewars-Solutions/6 kyu/6 kyu Your order, please.js
+++ codewars-Solutions/6 kyu/6 kyu Your order, please.js
@@ -7,3 +7,9 @@ function order(words){
     const split = words.split(' ').map( word => [word.replace(/[a-z]/gi, ''), word])
     return split.sort((a,b) => a[0][0] - b[0][0]).map( word => word[1]).join(' ')
   }
+
+// Можно еще так сделать
+function order(words){
+  const getNum = str => +str.split(/[^0-9]/gi).filter(x => x)[0]
+  return words.split(' ').sort((a, b) => getNum(a) - getNum(b)).join(' ')
+}
