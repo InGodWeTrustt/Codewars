d259a9350251ddc925b649ec6c22169d236c2037
InGodWeTrustt <fanat2016@mail.ru>
2023-01-16 15:48:21 +0300

    8 kyu Switch it Up!.js

---
+++ codewars-Solutions/8 kyu/8 kyu Switch it Up!.js
@@ -0,0 +1,9 @@
+function switchItUp(number) {
+    const digits = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+    return digits[number]
+}
+
+
+console.log(switchItUp(1),"One");
+console.log(switchItUp(3),"Three");
+console.log(switchItUp(5),"Five");
