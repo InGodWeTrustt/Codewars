7a366be2c6f25d16c75aa6e7384552551ebc1e1d
InGodWeTrustt <fanat2016@mail.ru>
2023-01-18 16:33:47 +0300

    7 kyu Remove anchor from URL.js

---
+++ codewars-Solutions/7 kyu/7 kyu Remove anchor from URL.js
@@ -0,0 +1,5 @@
+const removeUrlAnchor = url => url.split('#')[0]
+
+console.log(removeUrlAnchor('www.codewars.com#about'), 'www.codewars.com')
+console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'), 'www.codewars.com/katas/?page=1')
+console.log(removeUrlAnchor('www.codewars.com/katas/'), 'www.codewars.com/katas/')
