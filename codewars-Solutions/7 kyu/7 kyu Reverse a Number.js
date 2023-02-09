54527fcf3803d0c4934d725a1ee1245fe5a29fb6
InGodWeTrustt <fanat2016@mail.ru>
2023-02-10 00:11:27 +0300

    7 kyu Reverse a Number.js

---
+++ codewars-Solutions/7 kyu/7 kyu Reverse a Number.js
@@ -0,0 +1,16 @@
+function reverseNumber(n) {
+    if (/^[0-9]+/.test(n)) {
+        return Number(String(n).split('').reverse().join(''))
+    } else {
+        return Number('-' + String(n).slice(1).split('').reverse().join(''))
+    }
+}
+
+// Example
+console.log(reverseNumber(123), 321)
+console.log(reverseNumber(-123), -321, 'Negative Numbers should be preserved')
+console.log(reverseNumber(1000), 1, 'Should handle numbers ending with "0"')
+console.log(reverseNumber(4321234), 4321234)
+console.log(reverseNumber(5), 5)
+console.log(reverseNumber(0), 0)
+console.log(reverseNumber(98989898), 89898989)
