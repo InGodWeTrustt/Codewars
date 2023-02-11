199e589c8989a779dabc8fc809a2b11f38f3c3a3
InGodWeTrustt <fanat2016@mail.ru>
2023-02-11 22:48:04 +0300

    7 kyu You are a Cube!.js

---
+++ codewars-Solutions/7 kyu/7 kyu You are a Cube!.js
@@ -0,0 +1,24 @@
+function youAreACube(value) {
+    return Number.isInteger(Math.cbrt(value));
+}
+
+/** Explanation
+     
+                2        3
+   V = S * h = a  * a = a
+   where V -  Volume of a Cube
+         S - area of ​​a square because the base is a square (a * a)
+         a - side length of the cube
+
+               ______
+            3 /\  /
+  Then, a = \/  \/
+ * */ 
+
+
+// Example
+console.log(youAreACube(27), true);
+console.log(youAreACube(1), true);
+console.log(youAreACube(2), false);
+console.log(youAreACube(99), false);
+console.log(youAreACube(64), true); 
