bb917df9f0ca52da325f22706e31e100f85b054d
InGodWeTrustt <fanat2016@mail.ru>
2023-04-21 01:17:52 +0300

    8 kyu Kata Example Twist.js

---
+++ codewars-Solutions/8 kyu/8 kyu Kata Example Twist.js
@@ -0,0 +1,6 @@
+// add the value "codewars" to the websites array 1,000 times
+var websites = Array(1000).fill('codewars');
+
+console.log(websites.length > 0);
+console.log(websites.length == 1000);
+console.log(websites.every(function(e){return e == 'codewars';}));
