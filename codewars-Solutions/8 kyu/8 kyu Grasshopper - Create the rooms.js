b2ec3127bde9e65a2fac8e644a1be1a69f604cf5
InGodWeTrustt <fanat2016@mail.ru>
2023-05-29 23:16:20 +0300

    8 kyu Grasshopper - Create the rooms.js

---
+++ codewars-Solutions/8 kyu/8 kyu Grasshopper - Create the rooms.js
@@ -0,0 +1,22 @@
+// Add rooms here
+var rooms = {
+    r1: {
+        name: 'Room 1',
+        description: 'This is the first room',
+        completed: true
+    },
+    r2: {
+
+        name: 'Room 2',
+        description: 'This is the second room',
+        completed: true
+
+    },
+    r3: {
+
+        name: 'Room 3',
+        description: 'This is the third room',
+        completed: true
+
+    },
+}
